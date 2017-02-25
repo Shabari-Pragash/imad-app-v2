e27b852bb477104715fe0e19e8287c5fce9e02b5
Shabari-Pragash <shabari.pragash123@gmail.com>
2017-02-26 05:21:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,6 +84,10 @@ app.get('/:articleName',function(req,res) {
    //res.send('Article-one is served here');
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
