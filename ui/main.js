6ef49482151aeead2f2bfbac4786c1e3036247a7
Shabari-Pragash <shabari.pragash123@gmail.com>
2017-02-26 13:59:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ console.log('Loaded the page!');
 //var element=document.getElementById('madi');
 //element.innerHTML="new value";
 
-/*var img=document.getElementById("img");
+var img=document.getElementById("img");
 
 move=0;
 function moveRight()
@@ -15,13 +15,28 @@ function moveRight()
 img.onclick=function()
 {
     var interval=setInterval(moveRight,50);
-}*/
+}
 
 var click=document.getElementById("click");
 var span=document.getElementById("span");
 var count=0;
 click.onclick=function()
 {
-    count=count+1;
-    span.innerHTML=count.toString();
+    //create request
+    request=new XMLHttpRequest();
+    
+    request.onreadystatechange=function()
+    {
+        if(request.readystate==XMLHttpRequest.DONE)
+        {
+            var count=request.responseText;
+            span.innerHTML=count.toString();
+        }
+    }
+    
+    //Make request
+    request.open('GET','http://shabari-pragash.imad.hasura-app.io/counter',true);
+    request.send(null);
+    //count=count+1;
+    //span.innerHTML=count.toString();
 }
