0f8276315de68458545541ecc88dceee5edae2eb
Shabari-Pragash <shabari.pragash123@gmail.com>
2017-02-26 12:25:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,14 @@
 console.log('Loaded the page!');
 
-var element=document.getElementById('madi');
-element.innerHTML="new value";
+//var element=document.getElementById('madi');
+//element.innerHTML="new value";
 
 var img=document.getElementById("img");
 
 move=0;
 function moveRight()
 {
-    move=move+10;
+    move=move+5;
     img.style.marginLeft=move+'px';
 }
 
