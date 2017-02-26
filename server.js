817b7af5dd3c9fc05375edad634ef46c4ec9f249
Shabari-Pragash <shabari.pragash123@gmail.com>
2017-02-26 12:56:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ var count=0;
 app.get('/counter',function(req,res){
     count=count+1;
     res.send(count.toString());
-})
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
