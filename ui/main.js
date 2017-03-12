93ab2215ec4bf58a23b806d64a53ebb7e2c290de
Shabari-Pragash <shabari.pragash123@gmail.com>
2017-03-13 01:00:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -59,6 +59,39 @@ submit.onclick=function()
             if(request.status===200)
             {
                 //Capture request
+               alert('Logged in successfully');
+            }
+            else if(request.status==403)
+                 alert('Username/password is incorrect');
+            else if(request.status==500)
+                alert('Server Error');
+        }
+    }
+    
+    //Make request
+    var username=document.getElementById("username").value;
+    var password=document.getElementById("password").value;
+    request.open('POST','http://shabari-pragash.imad.hasura-app.io/login',true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({username:username,password:password}));
+    
+}
+
+var submit=document.getElementById("login");
+//var txt=document.getElementById("name").value;
+submit.onclick=function()
+{
+    //Make request
+    //create request
+    var request=new XMLHttpRequest();
+    
+    request.onreadystatechange=function()
+    {
+        if(request.readyState===XMLHttpRequest.DONE)
+        {
+            if(request.status===200)
+            {
+                //Capture request
                 var names=request.responseText;
                 names=JSON.parse(names);
                 var list='';
