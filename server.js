0beffc54315e588e3fceb97a33f354619e377313
Shabari-Pragash <shabari.pragash123@gmail.com>
2017-03-12 15:19:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,12 +87,28 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/test',function(req,res){
+/*app.get('/test',function(req,res){
    pool.query('SELECT * FROM ARTICLES',function(err,result){
       if(err)
         res.status(500).send(err.toString());
       else
         res.send(JSON.stringify(result.rows));
+   }); 
+});*/
+
+app.get('/articles/:articleName',function(req,res){
+   pool.query('SELECT * FROM ARTICLES WHERE TITLE=',function(err,result){
+      if(err)
+        res.status(500).send(err.toString());
+      else{
+          if(result.rows.length===0)
+            res.status(404).send(err.toString());
+          else
+          {
+              var articleData=result.rows[0];
+              res.send(createTemplate(articleData));
+          }
+      }
    }); 
 });
 
