7cd7bf7ca86e8504fc23de26ab824966acefbbe5
Shabari-Pragash <shabari.pragash123@gmail.com>
2017-03-12 21:56:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -103,7 +103,7 @@ app.post('/createuser',function(req,res){
     var password=req.body.password;
     var salt=crypto.randomBytes(128).toString('hex');
     var hashdb=hash(password,salt);
-    pool.query("INSERT INTO user (username,password) VALUES($1,$2)",[username,hashdb],function(err,result){
+    pool.query('INSERT INTO "user" (username,password) VALUES($1,$2)',[username,hashdb],function(err,result){
        if(err)
             res.status(500).send(err.toString());
         else
