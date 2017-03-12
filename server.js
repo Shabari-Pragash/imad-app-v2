002ce946616524201e7ffe81555c4520c590cd48
Shabari-Pragash <shabari.pragash123@gmail.com>
2017-03-12 22:32:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -116,7 +116,7 @@ app.post('/login',function(req,res){
     var password=req.body.password;
     var salt=crypto.randomBytes(128).toString('hex');
     var hashdb=hash(password,salt);
-    pool.query('SELECT * FROM "user" WHERE username=$1',[username],function(err,result){
+    pool.query('SELECT * FROM "user" WHERE username=$1 && password=$2',[username,hashdb],function(err,result){
        if(err)
             res.status(500).send(err.toString());
         else
