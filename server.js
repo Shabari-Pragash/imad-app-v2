7fe6b45864561f441c59b03011b1296a62a5e837
Shabari-Pragash <shabari.pragash123@gmail.com>
2017-03-12 22:24:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -122,7 +122,7 @@ app.post('/login',function(req,res){
         else
         {
             if(result.rows.length===0)
-                res.send('User invalid');
+                res.status(404).send(err.toString());
             else
                 res.send('User valid');
         }    
